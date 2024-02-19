5295ce9bef4a1ff69b5c712530202c896b19dfe4
OumaArera <johnouma999@gmail.com>
2024-02-20 02:16:36 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'; // Import Routes
+import NavBar from './assets/LandingPage/NavBar';
+import Login from './assets/LandingPage/Login';
+import Register from './assets/LandingPage/Register';
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div id="nav-bar">
+        <header className="header">
+          <NavLink to={'/'}>
+          </NavLink>
+          <NavBar />
+        </header>
+        <Routes> 
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} /> */}
+          <Route path="/signin" element={<Login />} /> 
+          <Route path="/signup" element={<Register />} /> 
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
-export default App
+export default App;
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import Login from './assets/LandingPage/Login.jsx'
+// import Register from './assets/LandingPage/Register.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* <Login />
+    <Register /> */}
   </React.StrictMode>,
 )
