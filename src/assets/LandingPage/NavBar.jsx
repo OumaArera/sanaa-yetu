6512eb1f9656e0bdcd9da834705a04107d01ba8f
OumaArera <johnouma999@gmail.com>
2024-02-20 02:12:34 +0300

    feat: navigation bar

---
+++ src/assets/LandingPage/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+
+const NavBar = () =>{
+    return (
+        <div className="navBar">
+          
+              <NavLink to={"/"} >Home</NavLink>
+            
+              <NavLink to={"/about"}>About</NavLink>
+    
+              <NavLink to={"/signin"}>Signin</NavLink>
+    
+              <NavLink to={"/signup"}>Signup</NavLink>
+           
+        </div>
+      );
+}
+
+export default NavBar;
+
