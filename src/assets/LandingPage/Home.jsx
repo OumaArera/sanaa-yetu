9c01a028f62cf4ba56565f9fa3b220b6b55aa424
Monica <monica@INFINIX-A3SK9UM.localdomain>
2024-02-26 02:47:40 -0800

    worked on the sliders

--- src/assets/LandingPage/Home.jsx
+++ src/assets/LandingPage/Home.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import image1 from '../Images/image1.jpeg';
-import image2 from '../Images/image2.jpg'
-import image3 from '../Images/image3.jpg'
-import image4 from '../Images/image4.jpg'
-import image5 from '../Images/image5.jpg'
+import React, { useState, useEffect } from 'react';
+import image11 from '../Images/image11.jpg';
+import image17 from '../Images/image17.jpg';
+import image13 from '../Images/image13.jpg';
+import image14 from '../Images/image14.jpg';
+import './Slider.css'; 
 
-import './Home.css';
+const Slider = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [image11, image17, image13, image14];
+  const messages = [
+   <div className = 'heading'>
+    Welcome to SanaaYetu
+   </div>, 
+    'Discover a treasure trove of unique artisanal products lovingly crafted by talented artisans from around Kenya. Whether you\'re searching for exquisite textiles, finely crafted woodworks, shimmering jewelry, or captivating pottery, our platform brings together a vibrant community of artisans and buyers, celebrating the beauty of handmade creations.',
+    'Why Shop with SanaaYetu: At SanaaYetu, we\'re committed to providing you with high-quality artisanal products that tell a story. With each purchase, you\'re not just buying a product; you\'re supporting talented artisans and their communities, preserving traditional craftsmanship, and contributing to sustainable livelihoods.',
+    'Contact Us: If you have any questions or inquiries, feel free to reach out to us. You can contact us via email at contact@sanaayetu.com or call us at +123456789.',
+    
+  ];
 
-const Home = () => {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [images.length]);
+
   return (
-    <div className="container">
-      <h1 className="title">
-         SanaaYetu Marketplace!
-      </h1>
-      <img className="image" src={image1} alt="page info home" />
-      <img className="image" src={image2} alt="page info home" />
-      <img className="image" src={image3} alt="page info home" />
-      <img className="image" src={image4} alt="page info home" />
-      <div className="card custom-style">
-        <img className="image" src={image5} alt="page info home" />
-        <div className="card-info">
-          <p>SanaaYetu Marketplace is a platform empowering artisans to showcase and sell their crafts while connecting them with buyers through an intuitive interface.</p>
+    <div className="slider-container">
+      <div className="slider-card">
+        <img className="slider-image" src={images[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} />
+        <div className="message-overlay">
+          <h1>{messages[currentImageIndex]}</h1>
         </div>
       </div>
-       
     </div>
   );
-}
+};
 
-export default Home;
+export default Slider;
